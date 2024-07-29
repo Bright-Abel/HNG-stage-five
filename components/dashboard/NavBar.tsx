7278d198da738f739b9c48407cd48e4d377ff88c
Abel Bright <oluwasegunbarnabas@gmail.com>
2024-07-29 16:15:39 +0100

    fix nav routing

--- components/dashboard/NavBar.tsx
+++ components/dashboard/NavBar.tsx
@@ -47,6 +47,8 @@ function NavBar() {
             className="text-[#633CFF] text-[16px] leading-[24px] font-semibold hidden md:block"
           >
             Preview
+          </Link>
+          <Link href="/preview">
             <Image src={prevLog} alt="logo" className="md:hidden block" />
           </Link>
         </div>
